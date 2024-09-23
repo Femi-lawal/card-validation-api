915475ad119f49b6cc0ef89a220157ba0dfa6121
Femi Lawal <femilawal76@gmail.com>
2024-09-23 14:40:14 -0400

    fix: added better error reporting for body parser

--- src/middleware/parseBody.js
+++ src/middleware/parseBody.js
@@ -7,31 +7,39 @@ export default (req, res, next) => {
 
         // Validate the presence of required fields in the XML body
         const requiredFields = ['creditcardnumber', 'expirationdate', 'cvv2', 'email', 'mobile', 'phonenumber'];
-        const hasAllFields = requiredFields.every(field => oldBody?.[field]);
-        console.log('hasAllFields', hasAllFields);
-        if (hasAllFields) {
-            // Map old XML body to a new JSON structure dynamically
-            const fieldMappings = {
-                creditcardnumber: 'creditCardNumber',
-                expirationdate: 'expirationDate',
-                cvv2: 'cvv2',
-                email: 'email',
-                mobile: 'mobile',
-                phonenumber: 'phoneNumber',
-            };
+        const missingFields = requiredFields.filter(field => !oldBody?.[field]);
 
-            const newBody = {};
-            for (const [oldKey, newKey] of Object.entries(fieldMappings)) {
-                newBody[newKey] = Array.isArray(oldBody[oldKey]) ? oldBody[oldKey][0] : oldBody[oldKey];
-            }
-            newBody.isXml = true;
-
-            req.body = newBody; // Assign the new structured body
-        } else {
-            // Return an error if the XML structure is not as expected
-            return res.status(400).json({ error: 'Invalid XML structure' });
+        if (missingFields.length > 0) {
+            // Return an error if required fields are missing
+            return res.status(400).json({
+                error: 'Invalid XML structure',
+                message: `The following required fields are missing: ${missingFields.join(', ')}. Please ensure all required fields are provided.`
+            });
         }
+
+        // Map old XML body to a new JSON structure dynamically
+        const fieldMappings = {
+            creditcardnumber: 'creditCardNumber',
+            expirationdate: 'expirationDate',
+            cvv2: 'cvv2',
+            email: 'email',
+            mobile: 'mobile',
+            phonenumber: 'phoneNumber',
+        };
+
+        const newBody = {};
+        for (const [oldKey, newKey] of Object.entries(fieldMappings)) {
+            newBody[newKey] = Array.isArray(oldBody[oldKey]) ? oldBody[oldKey][0] : oldBody[oldKey];
+        }
+        newBody.isXml = true;
+
+        req.body = newBody; // Assign the new structured body
+    } else {
+        return res.status(400).json({
+            error: 'Invalid Content-Type',
+            message: `Expected 'Content-Type' header to end with 'xml', but got '${contentType}' instead.`
+        });
     }
+
     next(); // Proceed to the next middleware
 };
-
